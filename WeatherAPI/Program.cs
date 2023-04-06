8af96eb76027aa6dc73b596bfb12350b8333ee7a
Caleb Schmidt <cschmi14@gmail.com>
2023-04-06 09:40:41 -0400

    Add project files.

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+
+namespace WeatherAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            
+        }
+    }
+}
